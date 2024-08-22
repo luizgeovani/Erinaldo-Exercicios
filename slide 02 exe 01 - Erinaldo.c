5c77c4912a22047bd09f3555e4fd4a0d16098c1a
luizgeovani <150821093+luizgeovani@users.noreply.github.com>
2024-08-21 23:22:13 -0300

    Add files via upload

---
+++ slide 02 exe 01 - Erinaldo.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main (void)
+//exercicio 01 slide 2 "linguagens e tecnicas de prog. prof erinaldo
+//apresente a mensagem "testando um software"
+{
+printf("testando um software")	;
+}
+
